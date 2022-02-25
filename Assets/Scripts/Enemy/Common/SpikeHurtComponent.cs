84b598249573a379eb9315e2a6f93f2a87030b90
IAymat <99961714+IAymat@users.noreply.github.com>
2022-02-25 12:49:39 +0100

    script daño del pinchote

---
+++ Assets/Scripts/Enemy/Common/SpikeHurtComponent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeHurtComponent : MonoBehaviour
+{
+	#region parameters
+	[SerializeField]
+	private float _touchDamage = 10;
+	#endregion
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        PlayerLife player = collision.gameObject.GetComponent<PlayerLife>();
+
+        if(player != null)
+		{
+			player.gameObject.GetComponent<PlayerLife>().SetHealth(-_touchDamage);
+		}
+	}
+
+
+}
