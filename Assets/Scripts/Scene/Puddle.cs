a50ac82d45d33f9bb1b415e76f3a812ac08b26f3
IAymat <99961714+IAymat@users.noreply.github.com>
2022-03-01 18:38:38 +0100

    para cundo fregona activa

--- Assets/Scripts/Scene/Puddle.cs
+++ Assets/Scripts/Scene/Puddle.cs
@@ -7,6 +7,7 @@ public class Puddle : MonoBehaviour
 	#region parameters
 	[SerializeField]
 	private float _touchHydrate = 10;
+	private float _useFregona = 20;
 	#endregion
 
 	#region references
@@ -28,8 +29,21 @@ public class Puddle : MonoBehaviour
 		{
 			player.SetHealth(+_touchHydrate);
 			_spriteRenderer.sprite = newSprite;
+			GetComponent<Collider2D>().enabled = false;
 			Destroy(gameObject, 3f);
 		}
 	}
-
+	/*
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		PlayerLife player = collision.gameObject.GetComponent<PlayerLife>();
+		if (player != null && "FREGONA ACTIVA")
+		{
+			GetComponent<Collider2D>().enabled = false;
+			player.SetHealth(+_useFregona);
+			_spriteRenderer.sprite = newSprite;
+			Destroy(gameObject, 3f);
+		}
+	}
+	*/
 }
