581098b65f5922c5edbc318b21b33eff86e59bea
DavidRainder <davirive@ucm.es>
2022-03-01 23:36:40 +0100

    pequeño cambio de comentario

--- Assets/Scripts/Items + Inventory/Inventory.cs
+++ Assets/Scripts/Items + Inventory/Inventory.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -77,4 +77,4 @@ public class InventorySlot
 public class ItemToInstantiate
 {
     public ScriptableObject _itemPrefabToInstantiate;
-}*/
+}
