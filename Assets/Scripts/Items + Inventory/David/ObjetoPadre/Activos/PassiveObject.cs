f89a6ba4e985bd8cd2bbe84b1c05bae459784f08
DavidRainder <davirive@ucm.es>
2022-03-02 11:27:50 +0100

    pequeños cambios de scripts de objetos

---
+++ Assets/Scripts/Items + Inventory/David/ObjetoPadre/Activos/PassiveObject.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveObject : Object
+{
+    //Meter atributos aquí
+    public void Activate()
+    {
+        Debug.Log("Objeto " + gameObject.name + " activado");
+    }
+}
+/*public Item(ItemObject item)
+{
+    Name = item.name;
+    itemID = item.itemID;
+    sprite = item.sprite;
+    itemPrefab = item.itemPrefab;
+    buffs = new ItemBuff[item.buffs.Length];
+
+    for(int i = 0; i < buffs.Length; i++)
+    {
+        buffs[i] = new ItemBuff(item.buffs[i].value);
+    }
+}
+}
+
+[System.Serializable]
+public class ItemBuff
+{
+public ItemAttributes attribute;
+public int value;
+public ItemBuff(int _value)
+{
+    value = _value;
+}
+}*/
--- Assets/Scripts/Items + Inventory/David/ObjetoPadre/Object.cs
+++ Assets/Scripts/Items + Inventory/David/ObjetoPadre/Object.cs
@@ -5,34 +5,12 @@ using UnityEngine;
 public enum ItemTypes { Buff, NoBuff,Active }
 
 public class Object : MonoBehaviour
-{
+{ 
     public int ID;
-
-    
+    public Sprite itemSprite; //para qué queremos el sprite ;(
+    public ItemTypes type;
+    public string nameOnScreen;
+    public string littleDescriptionOnScreen;
+    public string descriptionOnInventoryMenu;
 }
 
-    /*    public Item(ItemObject item)
-    {
-        Name = item.name;
-        itemID = item.itemID;
-        sprite = item.sprite;
-        itemPrefab = item.itemPrefab;
-        buffs = new ItemBuff[item.buffs.Length];
-
-        for(int i = 0; i < buffs.Length; i++)
-        {
-            buffs[i] = new ItemBuff(item.buffs[i].value);
-        }
-    }
-}
-
-[System.Serializable]
-public class ItemBuff
-{
-    public ItemAttributes attribute;
-    public int value;
-    public ItemBuff(int _value)
-    {
-        value = _value;
-    }
-}*/
