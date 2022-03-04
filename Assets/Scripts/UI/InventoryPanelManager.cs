a1d6744f2440b467ea171cf68132ac174ee0939b
almasso <almasso@ucm.es>
2022-03-04 16:41:50 +0100

    Primera versión inventario activos

--- Assets/Scripts/UI/InventoryPanelManager.cs
+++ Assets/Scripts/UI/InventoryPanelManager.cs
@@ -6,47 +6,81 @@ using UnityEngine.UI;
 public class InventoryPanelManager : MonoBehaviour
 {
     #region parameters
-    [SerializeField] private int _spaceBetweenItemsX, _spaceBetweenItemsY, _columns, _xStart, _yStart;
-    [SerializeField] private GameObject inventoryPrefab;
-    List<GameObject> itemsDisplayed = new List<GameObject>();
+    [SerializeField] private int _spaceBetweenItemsX, _spaceBetweenItemsY, _columns, _xStart, _yStart, _activeXPos, _activeYPos;
+    [SerializeField] private GameObject _passiveInventoryPrefab, _activeInventoryPrefab;
+    List<GameObject> passiveItemsDisplayed = new List<GameObject>();
+    [HideInInspector]public GameObject activeItemDisplayed;
     #endregion
 
-    public void CreateDisplay()
+    public void CreatePassiveDisplay()
     {
-        for (int i = 0; i < InventoryTry2.Instance.passiveItemList.Count; i++)
+        for (int i = 0; i < Inventory.Instance.passiveItemList.Count; i++)
         {
-            var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
-            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-            itemsDisplayed.Add(InventoryTry2.Instance.passiveItemList[i]);
+            var obj = Instantiate(_passiveInventoryPrefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPassiveSlotPosition(i);
+            passiveItemsDisplayed.Add(Inventory.Instance.passiveItemList[i]);
         }
     }
 
-    public void UpdateDisplay()
+    public void CreateActiveDisplay()
     {
-        for(int i = 0; i < InventoryTry2.Instance.passiveItemList.Count; i++)
+        if(activeItemDisplayed != null)
         {
-            if(!itemsDisplayed.Contains(InventoryTry2.Instance.passiveItemList[i]))
+            var obj2 = Instantiate(_activeInventoryPrefab, Vector3.zero, Quaternion.identity, transform);
+            obj2.GetComponent<RectTransform>().localPosition = GetActiveSlotPosition();
+            activeItemDisplayed = Inventory.Instance.activeItem;
+        }
+    }
+
+    public void UpdatePassiveDisplay()
+    {
+        for(int i = 0; i < Inventory.Instance.passiveItemList.Count; i++)
+        {
+            if(!passiveItemsDisplayed.Contains(Inventory.Instance.passiveItemList[i]))
             {
-                var obj = Instantiate(inventoryPrefab, Vector3.zero, Quaternion.identity, transform);
-                obj.transform.GetChild(0).GetComponentInChildren<Image>().sprite = InventoryTry2.Instance.passiveItemList[i].GetComponent<SpriteRenderer>().sprite;
-                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
-                itemsDisplayed.Add(InventoryTry2.Instance.passiveItemList[i]);
+                var obj = Instantiate(_passiveInventoryPrefab, Vector3.zero, Quaternion.identity, transform);
+                obj.transform.GetChild(0).GetComponentInChildren<Image>().sprite = Inventory.Instance.passiveItemList[i].GetComponent<SpriteRenderer>().sprite;
+                obj.GetComponent<RectTransform>().localPosition = GetPassiveSlotPosition(i);
+                passiveItemsDisplayed.Add(Inventory.Instance.passiveItemList[i]);
             }
         }
     }
 
-    public Vector3 GetPosition(int i)
+    public void UpdateActiveDisplay()
+    {
+        if(activeItemDisplayed != Inventory.Instance.activeItem && activeItemDisplayed == null)
+        {
+            var obj2 = Instantiate(_activeInventoryPrefab, Vector3.zero, Quaternion.identity, transform);
+            obj2.transform.GetChild(0).GetComponentInChildren<Image>().sprite = Inventory.Instance.activeItem.GetComponent<SpriteRenderer>().sprite;
+            obj2.GetComponent<RectTransform>().localPosition = GetActiveSlotPosition();
+            activeItemDisplayed = Inventory.Instance.activeItem;
+        }
+        else if(activeItemDisplayed != Inventory.Instance.activeItem && activeItemDisplayed != null)
+        {
+            activeItemDisplayed = Inventory.Instance.activeItem;
+            _activeInventoryPrefab.GetComponentInChildren<Image>().sprite = activeItemDisplayed.GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+
+    public Vector3 GetPassiveSlotPosition(int i)
     {
         return new Vector3(_xStart + (_spaceBetweenItemsX * (i % _columns)), _yStart + (-_spaceBetweenItemsY * (i / _columns)), 0f);
     }
 
+    public Vector3 GetActiveSlotPosition()
+    {
+        return new Vector3(_activeXPos, _activeYPos, 0f);
+    }
+
     void Start()
     {
-        CreateDisplay();
+        CreatePassiveDisplay();
+        CreateActiveDisplay();
     }
 
     void Update()
     {
-        UpdateDisplay();
+        UpdatePassiveDisplay();
+        UpdateActiveDisplay();
     }
 }
