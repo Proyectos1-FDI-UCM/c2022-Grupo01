7146823d727f77294e06b53e07e7852665808909
DavidRainder <davirive@ucm.es>
2022-03-07 00:09:34 +0100

    Cambio de estructura + cambio en objetos activos

--- Assets/Scripts/Items/Bolsa de Hielo/BolsaDeHieloActivo.cs
+++ Assets/Scripts/Items/Bolsa de Hielo/BolsaDeHieloActivo.cs
@@ -4,46 +4,33 @@ using UnityEngine;
 
 public class BolsaDeHieloActivo : ActiveObject
 {
-    #region parameters
-    [SerializeField] private GameObject _icebagPrefab;
-	[SerializeField] private float _iceForce = 20f;
-	[SerializeField] private float _cooldown = 40f;
-    #endregion
-
     #region properties
-    private float _elapsedTime = 0f;
-	private bool _canShoot = true;
-    #endregion
-    #region references
-    private Transform _shotPoint;
+    private bool pickedUp = false;
+    private GameObject bolsaDeHielo;
     #endregion
 
-    private void Start()
-	{
-		_shotPoint = PlayerManager.Instance.attackPoint.transform;
-		_elapsedTime = 0;
-	}
-	public override void Activate()
+    #region references
+    [SerializeField] GameObject iceBagPrefab;
+    #endregion
+
+    public override void Activate()
 	{
 		base.Activate();
-		if(_canShoot) LanzaHielo(); 
-	}
 
-    private void Update()
-    {
-		_elapsedTime += Time.deltaTime;
-		if (_elapsedTime >= _cooldown) _canShoot = true;
+        if (!pickedUp)
+        {
+            base.Activate();
+            bolsaDeHielo = new GameObject("BolsaDeHielo");
+            bolsaDeHielo.transform.parent = PlayerManager.Instance.player.transform;
+            bolsaDeHielo.AddComponent<BolsaDeHieloController>();
+            bolsaDeHielo.GetComponent<BolsaDeHieloController>()._icebagPrefab = iceBagPrefab;
+            pickedUp = true;
+        }
     }
 
-    void LanzaHielo()
-	{
-		_canShoot = false; 
-		_elapsedTime = 0;
-		_shotPoint = PlayerManager.Instance.attackPoint.transform;
-		GameObject nuevoIcebagPrefab = Instantiate(_icebagPrefab, _shotPoint.position, Quaternion.identity);
-		Debug.Log(nuevoIcebagPrefab.transform.position);
-		Rigidbody2D rb = nuevoIcebagPrefab.GetComponent<Rigidbody2D>();
-		rb.rotation = PlayerManager.Instance.gun.GetComponent<Rigidbody2D>().rotation;
-		rb.AddForce(_shotPoint.right * _iceForce, ForceMode2D.Impulse);
-	}
+    public override void ChangeActiveObject()
+    {
+        base.ChangeActiveObject();
+        Destroy(bolsaDeHielo);
+    }
 }
