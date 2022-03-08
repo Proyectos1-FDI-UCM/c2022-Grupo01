7b195d4ce0dcb058d398bc12ed903d35090109fd
lauraonetwo <98849496+LauraWangQiu@users.noreply.github.com>
2022-03-08 16:59:44 +0100

    transition script

--- Assets/Scripts/UI/Transition.cs
+++ Assets/Scripts/UI/Transition.cs
@@ -9,12 +9,27 @@ public class Transition : MonoBehaviour
 
     private Animator transitionAnimator;
 
+    [SerializeField] private GameObject _player;
+    [SerializeField] private float position_X = 0f;
+    [SerializeField] private float position_Y = 0f;
+    private Transform _myTransform;
+
     void Start()
     {
-        
+        _myTransform = transform;
     }
 
     void Update()
+    {
+        // Teletransporte del jugador dependiendo de la posición en la que acabe la sala
+        if (_myTransform.position.x == position_X && _myTransform.position.y == position_Y)
+        {
+            //_myTransform.position.x =;
+            //_myTransform.position.y =;
+        }
+    }
+
+    public void OnTriggerEnter2D (Collider2D collision)
     {
         
     }
