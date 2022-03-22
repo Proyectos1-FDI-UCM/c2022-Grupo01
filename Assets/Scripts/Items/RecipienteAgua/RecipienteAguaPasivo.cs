8836ba66311aa4648cfd28067eb92d79c3db7f06
IAymat <99961714+IAymat@users.noreply.github.com>
2022-03-22 19:56:19 +0100

    pasivo Recipiente de Agua +30 maxhealth y rellena normalhealth

---
+++ Assets/Scripts/Items/RecipienteAgua/RecipienteAguaPasivo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipienteAguaPasivo : PassiveObject
+{
+    #region methods
+    public override void Activate()
+    {
+        base.Activate();
+        PlayerManager.Instance.myLifeState = PlayerManager.LifeStates.Normal;
+    }
+    #endregion
+}
