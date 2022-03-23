9113acb3d9062b208258a3a33051669f31774b80
almasso <almasso@ucm.es>
2022-03-23 02:15:08 +0100

    Sala de reciclajes pero no se ven las papeleras

---
+++ Assets/Scripts/Recycling System/Bin.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BinType {Glass, Paper, Plastic}
+
+public class Bin : MonoBehaviour
+{
+    public BinType binType;
+}
--- Assets/Scripts/Recycling System/BinController.cs
+++ Assets/Scripts/Recycling System/BinController.cs
@@ -4,67 +4,58 @@ using UnityEngine;
 
 public class BinController : MonoBehaviour
 {
-    public enum BinType {Glass, Plastic, Cardboard }
     #region parameters
     [SerializeField] private float _interactionRange = 1f;
     [SerializeField] private LayerMask _objectLayer;
-    public BinType _binType;
     #endregion
 
     #region methods
-    void InteractWithBin(Collider2D bin)
+    void InteractWithBin(Collider2D collider)
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        Bin bin = collider.GetComponent<Bin>();
+
+        switch(bin.binType)
         {
-            switch(_binType)
-            {
-                case BinType.Glass:
-                    if(Inventory.Instance.bottleItem.GetComponent<BottleObject>().ID == 1)
-                    {
-                        PlayerManager.Instance.ChangePlayerShields(2);
-                    }
-                    else
-                    {
-                        PlayerManager.Instance.ChangePlayerLife(-10f);
-                    }
-                    break;
+            case BinType.Glass:
+                if(Inventory.Instance.bottleItem.GetComponent<BottleObject>().ID == 1)
+                {
+                    PlayerManager.Instance.ChangePlayerShields(2);
+                }
+                else
+                {
+                    PlayerManager.Instance.ChangePlayerLife(-10f);
+                }
+                break;
 
-                case BinType.Plastic:
-                    if(Inventory.Instance.bottleItem.GetComponent<BottleObject>().ID == 2)
-                    {
-                        PlayerManager.Instance.ChangePlayerLife(20f);
-                    }
-                    else
-                    {
-                        PlayerManager.Instance.ChangePlayerLife(-10f);
-                    }
-                    break;
+            case BinType.Plastic:
+                if(Inventory.Instance.bottleItem.GetComponent<BottleObject>().ID == 2)
+                {
+                    PlayerManager.Instance.ChangePlayerLife(20f);
+                }
+                else
+                {
+                    PlayerManager.Instance.ChangePlayerLife(-10f);
+                }
+                break;
 
-                case BinType.Cardboard:
-                    if (Inventory.Instance.bottleItem.GetComponent<BottleObject>().ID == 0)
-                    {
-                        PlayerManager.Instance.ChangeMaxLife(10);
-                    }
-                    else
-                    {
-                        PlayerManager.Instance.ChangePlayerLife(-10f);
-                    }
-                    break;
-            }
+            case BinType.Paper:
+                if (Inventory.Instance.bottleItem.GetComponent<BottleObject>().ID == 0)
+                {
+                    PlayerManager.Instance.ChangeMaxLife(10);
+                }
+                else
+                {
+                    PlayerManager.Instance.ChangePlayerLife(-10f);
+                }
+                break;    
         }
     }
     #endregion
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             Collider2D[] bins = Physics2D.OverlapCircleAll(transform.position, _interactionRange, _objectLayer);
             if (bins.Length >= 1) InteractWithBin(bins[0]);
