84fdb680f4adeee57c6572a34470f3d5d4490903
almasso <almasso@ucm.es>
2022-04-05 22:25:32 +0200

    Sonido de cofre abierto añadido e incorporado

--- Assets/Scripts/Scene/OpenChest.cs
+++ Assets/Scripts/Scene/OpenChest.cs
@@ -60,6 +60,7 @@ public class OpenChest : MonoBehaviour
                 _shadowObject.GetComponent<SpriteRenderer>().sprite = _newOpenedSprite;
                 Instantiate(_objectHeld, transform.position + _offset, Quaternion.identity);
                 _chestOpen = false;
+                FindObjectOfType<AudioManager>().Play("OpenedChest");
                 GameManager.Instance.itemList.RemoveAt(rnd);
             }
         }
